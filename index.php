03e62afea57f45f98f1332092077c5ecd624a314
zivkoss <zivcoss@yahoo.com>
2023-07-17 16:25:18 +0200

    new practice added 998.5

--- index.php
+++ index.php
@@ -1,25 +1,159 @@
 <?php
 
-$text = "HelloWorld!";
-$celi_broj = 11;
-$float_broj = 5.14;
-$tacno_netacno = false;
+$broj_1 = 11;
+$broj_2 = 5;
 
-var_dump($text);
+$rezultat = $broj_1 - $broj_2;
 
-echo "<br>";
-
- var_dump($celi_broj);
-
- echo "<br>";
-
- var_dump($float_broj);
-
- echo "<br>";
-
- var_dump($tacno_netacno);
+echo "$broj_1 minus $broj_2, jednako je: " . $rezultat;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// **************************************************
+
+// $text = "HelloWorld!";
+// $celi_broj = 11;
+// $float_broj = 5.14;
+// $tacno_netacno = false;
+// $prazno = NULL;
+
+// var_dump($text);
+
+// echo "<br>";
+
+//  var_dump($celi_broj);
+
+//  echo "<br>";
+
+//  var_dump($float_broj);
+
+//  echo "<br>";
+
+//  var_dump($tacno_netacno);
+
+//  echo "<br>";
+
+//  var_dump($prazno);
+
+
+ //*********************************************** 
 
 
 
