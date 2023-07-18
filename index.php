e685c885eeb4c6611b3b65c1f56852da7dc053c4
zivkoss <zivcoss@yahoo.com>
2023-07-18 17:53:31 +0200

    new validation  project added

--- index.php
+++ index.php
@@ -1,13 +1,87 @@
 <html>
 
 <head>
+    <title>Uloguj se</title>
+</head>
+
+<body>
+
+    <form method="POST">
+        <p>
+            <label for="">Ime</label>
+            <input type="text" name="fname">
+        </p>
+        <p>
+            <label for="">Prezime</label>
+            <input type="text" name="lname">
+        </p>
+        <p>
+            <label for="">Email</label>
+            <input type="email" name="email">
+        </p>
+        <p>
+            <label for="">Datumrodjenja</label>
+            <input type="date" name="date">
+        </p>
+        <button type="submit">Posalji</button>
+    </form>
+
+
+
+    <?php
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $first_name = $_POST['fname'];
+        $last_name = $_POST['lname'];
+        $email = $_POST['email'];
+        $date = $_POST['date'];
+
+        $today = date("Y-m-d");
+        $today = date_create($today);
+
+        $converted_date = date_create($date);
+
+        $diff = date_diff($converted_date, $today);
+        $diff = $diff->y;
+
+        if ($diff >= 18) {
+            $data = "Ime: $first_name,
+                 Prezime: $last_name,
+                 Email: $email,
+                 Datum rodjenja: $date";
+
+            $file = fopen($first_name . ".txt", "w");
+
+            fwrite($file, $data);
+
+            fclose($file);
+            
+            echo "Uspesno set poslali poruku";
+        } else {
+            echo "Morate imati 18 godina da bi ste poslali poruku?";
+        }
+    }
+
+    ?>
+
+</body>
+
+</html>
+
+
+
+
+
+<html>
+
+<head>
     <title>Auta</title>
 </head>
 
 <body>
 
     <?php
-    
+
     $selectedBrand = '';
 
     if (isset($_GET['brand'])) {
@@ -49,4 +123,3 @@
 </body>
 
 </html>
-
